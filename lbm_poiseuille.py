fd03f7cf2b081da8a5ba15c189a4a8d6856b7469
Michał Januszewski <spock@gentoo.org>
2009-11-11 21:34:28 +0100

    Add support for pressure-driven Poiseuille testcase (2D).

--- lbm_poiseuille.py
+++ lbm_poiseuille.py
@@ -25,18 +25,44 @@ class LBMGeoPoiseuille(geo.LBMGeo2D):
 				self.set_geo((0, i), self.NODE_WALL)
 				self.set_geo((self.lat_w-1, i), self.NODE_WALL)
 
+		if self.options.pressure:
+			if self.options.horizontal:
+				pressure = self.maxv * (8.0 * self.options.visc) / (self.get_chan_width()**2) * self.lat_w
+
+				for i in range(1, self.lat_h-1):
+					self.set_geo((0, i), self.NODE_PRESSURE, (1.0/3.0) - pressure/2.0)
+					self.set_geo((self.lat_w-1, i), self.NODE_PRESSURE, (1.0/3.0) + pressure/2.0)
+			else:
+				pressure = self.maxv * (8.0 * self.options.visc) / (self.get_chan_width()**2) * self.lat_h
+
+				for i in range(1, self.lat_w-1):
+					self.set_geo((i, 0), self.NODE_PRESSURE, (1.0/3.0) + pressure/2.0)
+					self.set_geo((i, self.lat_h-1), self.NODE_PRESSURE, (1.0/3.0) - pressure/2.0)
+
 	def init_dist(self, dist):
 		if self.options.static:
-			profile = self.get_velocity_profile()
+			if self.options.pressure:
+				pressure = self.maxv * (8.0 * self.options.visc) / (self.get_chan_width()**2)
 
-			if self.options.horizontal:
-				for y in range(0, self.lat_h):
-					self.velocity_to_dist((0, y), (profile[y], 0.0), dist)
-				self.fill_dist((0, slice(None)), dist)
+				if self.options.horizontal:
+					for x in range(0, self.lat_w):
+						self.velocity_to_dist((x, 0), (0.0, 0.0), dist, rho=(1.0 + 3.0 * pressure * (self.lat_w/2.0 - x)))
+						self.fill_dist((x, 0), dist, (x, slice(None)))
+				else:
+					for y in range(0, self.lat_h):
+						self.velocity_to_dist((0, y), (0.0, 0.0), dist, rho=(1.0 + 3.0 * pressure * (self.lat_h/2.0 - y)))
+						self.fill_dist((0, y), dist, (slice(None), y))
 			else:
-				for x in range(0, self.lat_w):
-					self.velocity_to_dist((x, 0), (0.0, profile[x]), dist)
-				self.fill_dist((slice(None), 0), dist)
+				profile = self.get_velocity_profile()
+
+				if self.options.horizontal:
+					for y in range(0, self.lat_h):
+						self.velocity_to_dist((0, y), (profile[y], 0.0), dist)
+					self.fill_dist((0, slice(None)), dist)
+				else:
+					for x in range(0, self.lat_w):
+						self.velocity_to_dist((x, 0), (0.0, profile[x]), dist)
+					self.fill_dist((slice(None), 0), dist)
 		else:
 			self.velocity_to_dist((0, 0), (0.0, 0.0), dist)
 			self.fill_dist((0, 0), dist)
@@ -83,6 +109,8 @@ class LPoiSim(lbm.LBMSim):
 		opts.append(optparse.make_option('--test', dest='test', action='store_true', default=False, help='generate test data'))
 		opts.append(optparse.make_option('--horizontal', dest='horizontal', action='store_true', default=False, help='use horizontal channel'))
 		opts.append(optparse.make_option('--static', dest='static', action='store_true', default=False, help='start with the correct velocity profile in the whole simulation domain'))
+		opts.append(optparse.make_option('--pressure', dest='pressure', action='store_true', default=False, help='use a pressure gradient instead of a body force to drive the flow'))
+
 
 		if defaults is not None:
 			defaults_ = defaults
@@ -92,19 +120,20 @@ class LPoiSim(lbm.LBMSim):
 		lbm.LBMSim.__init__(self, geo_class, misc_options=opts, args=args, defaults=defaults_)
 
 		if self.options.test or self.options.benchmark:
-			self.options.periodic_y = not self.options.horizontal
-			self.options.periodic_x = self.options.horizontal
-
 			self._init_geo()
 
-			if self.options.horizontal:
-				self.options.accel_x = geo_class.maxv * (8.0 * self.options.visc) / (self.geo.get_chan_width()**2)
-				if self.options.test:
-					self.add_iter_hook(self.options.max_iters-1, self.output_profile_horiz)
-			else:
-				self.options.accel_y = geo_class.maxv * (8.0 * self.options.visc) / (self.geo.get_chan_width()**2)
-				if self.options.test:
-					self.add_iter_hook(self.options.max_iters-1, self.output_profile_vert)
+			if not self.options.pressure:
+				self.options.periodic_y = not self.options.horizontal
+				self.options.periodic_x = self.options.horizontal
+
+				if self.options.horizontal:
+					self.options.accel_x = geo_class.maxv * (8.0 * self.options.visc) / (self.geo.get_chan_width()**2)
+					if self.options.test:
+						self.add_iter_hook(self.options.max_iters-1, self.output_profile_horiz)
+				else:
+					self.options.accel_y = geo_class.maxv * (8.0 * self.options.visc) / (self.geo.get_chan_width()**2)
+					if self.options.test:
+						self.add_iter_hook(self.options.max_iters-1, self.output_profile_vert)
 
 			if self.options.test:
 				self.add_iter_hook(1000, self.output_pars, every=True)
