99c856d500c88dc651123a07c906bdb1536c09d3
Michał Januszewski <spock@gentoo.org>
2009-12-21 22:50:29 +0100

    Add a dummy backend for unit tests.

---
+++ backend_dummy.py
@@ -0,0 +1,44 @@
+import pycuda.autoinit
+import pycuda.compiler
+import pycuda.driver as cuda
+
+from struct import calcsize, pack
+
+class DummyBackend(object):
+
+	def __init__(self):
+		self.buffers = {}
+
+	def alloc_buf(self, size=None, like=None):
+		return 0
+
+	def to_buf(self, cl_buf, source=None):
+		pass
+
+	def from_buf(self, cl_buf, target=None):
+		pass
+
+	def build(self, source):
+		pass
+
+	def get_kernel(self, prog, name, block, args, args_format, shared=None):
+		return None
+
+	def run_kernel(self, kernel, grid_size):
+		return None
+
+	def sync(self):
+		pass
+
+	def get_defines(self):
+		return {
+			'shared_var': '__shared__',
+			'kernel': '__global__',
+			'global_ptr': '',
+			'const_ptr': '',
+			'device_func': '__device__',
+			'const_var': '__constant__',
+		}
+
+
+backend=DummyBackend
