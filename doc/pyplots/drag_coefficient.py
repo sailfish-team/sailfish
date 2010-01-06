d5a6a289fd28e0a81d97886f7cf317844c1de4c3
Michał Januszewski <spock@gentoo.org>
2010-01-06 21:08:35 +0100

    Fix the plots so that the axis labels are always visible.

--- doc/pyplots/drag_coefficient.py
+++ doc/pyplots/drag_coefficient.py
@@ -1,23 +1,30 @@
 import os
 
-from pylab import *
+import pylab
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
 
 def make_subplot(file, title_):
-	data = csv2rec(file, delimiter=' ', names=('re', 'sim', 'th'))
-	loglog(data['re'], data['sim'], 'bo-')
-	loglog(data['re'], data['th'], 'ro-')
-	grid('.')
-	grid(which='minor')
-	xlim(data[0][0], data[-1][0])
-	ylabel('drag coefficient')
-	xlabel('Reynolds number')
-	title(title_)
+    data = pylab.csv2rec(file, delimiter=' ', names=('re', 'sim', 'th'))
+    figW = 5.5
+    figH = 4.5
+    fig = plt.figure(subplotpars=mpl.figure.SubplotParams(left=0.125, bottom=0.130))
+    ax = fig.add_subplot(111)
+
+    pl_sim = ax.loglog(data['re'], data['sim'], 'bo-')
+    pl_th = ax.loglog(data['re'], data['th'], 'ro-')
+    ax.legend((pl_sim, pl_th), ('simulation results', 'theoretical approximation'), 'best')
+    ax.grid('.')
+    ax.grid(which='minor')
+    ax.set_xlim(data[0][0], data[-1][0])
+    ax.set_ylabel('drag coefficient')
+    ax.set_xlabel('Reynolds number')
+    ax.set_title(title_)
 
 def make_plot(basepath, files, titles):
     for i, sp in enumerate(zip(files, titles)):
-        figure(i+1)
         make_subplot(os.path.join(basepath, sp[0]), sp[1])
-    show()
 
 
 
--- doc/pyplots/poiseuille.py
+++ doc/pyplots/poiseuille.py
@@ -3,14 +3,15 @@ import os
 from pylab import *
 
 def make_subplot(file, title_):
-	data = csv2rec(file, delimiter=' ', names=('visc', 'y'))
-	semilogx(data['visc'], data['y'], 'bo-')
-	grid('.')
-	grid(which='minor')
-	xlim(data[0][0], data[-1][0])
-	ylabel('max velocity / theoretical max velocity - 1')
-	xlabel('viscosity')
-	title(title_)
+    data = csv2rec(file, delimiter=' ', names=('visc', 'y'))
+    subplots_adjust(left=0.16, bottom=0.13, right=0.95)
+    semilogx(data['visc'], data['y'], 'bo-')
+    grid('.')
+    grid(which='minor')
+    xlim(data[0][0], data[-1][0])
+    ylabel('max velocity / theoretical max velocity - 1')
+    xlabel('viscosity')
+    title(title_)
 
 
 def make_plot(basepath, files, titles):
