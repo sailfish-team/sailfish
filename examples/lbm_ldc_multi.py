dc55393d69b2509c681befa662d4ec5b0dda1df0
Michał Januszewski <spock@gentoo.org>
2011-05-31 10:19:56 +0200

    Add block splitting to the the multi-GPU LDC example.

--- examples/lbm_ldc_multi.py
+++ examples/lbm_ldc_multi.py
@@ -1,10 +1,40 @@
 #!/usr/bin/python
 
+import math
 import numpy as np
 from sailfish.geo import LBGeometry2D
 from sailfish.geo_block import LBBlock2D, GeoBlock2D
 from sailfish.controller import LBSimulationController
 from sailfish.lb_single import LBFluidSim, LBForcedSim
+
+class LDCGeometry(LBGeometry2D):
+    def blocks(self, n=None):
+        blocks = []
+        bps = int(math.sqrt(self.config.blocks))
+
+        if bps**2 != self.config.blocks:
+            print ('Only configutaions with '
+                    'square-of-interger numbers of blocks are supported. '
+                    'Falling back to {} x {} blocks.'.format(bps, bps))
+
+        yq = self.gy / bps
+        ydiff = self.gy % bps
+        xq = self.gx / bps
+        xdiff = self.gx % bps
+
+        for i in range(0, bps):
+            xsize = xq
+            if i == bps:
+                xsize += xdiff
+
+            for j in range(0, bps):
+                ysize = yq
+                if j == bps:
+                    ysize += ydiff
+
+                blocks.append(LBBlock2D((i * xq, j * yq), (xsize, ysize)))
+
+        return blocks
 
 
 class LDCBlock(GeoBlock2D):
@@ -42,5 +72,5 @@ class LDCSim(LBFluidSim, LBForcedSim):
 
 
 if __name__ == '__main__':
-    ctrl = LBSimulationController(LDCSim)
+    ctrl = LBSimulationController(LDCSim, LDCGeometry)
     ctrl.run()
