f0816973bc02115496cb2fb9065a4f84c40cb5b4
Michał Januszewski <michalj@gmail.com>
2011-08-08 02:05:33 +0200

    Add initial set of benchmark scripts.

---
+++ benchmark/3d_2blocks.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+from examples.lbm_ldc_multi_3d import LDCSim
+from sailfish.controller import LBSimulationController
+from sailfish.geo import LBGeometry3D
+from sailfish.geo_block import LBBlock3D
+
+import util
+
+class BenchmarkXConnGeometry(LBGeometry3D):
+    def blocks(self, n=None):
+        w = self.gx / 2
+        blocks = [LBBlock3D((0, 0, 0), (w, self.gy, self.gz)),
+                LBBlock3D((w, 0, 0), (w, self.gy, self.gz))]
+        print w, self.gy, self.gz
+        return blocks
+
+
+class BenchmarkYConnGeometry(LBGeometry3D):
+    def blocks(self, n=None):
+        h = self.gy / 2
+        blocks = [LBBlock3D((0, 0, 0), (self.gx, h, self.gz)),
+                LBBlock3D((0, h, 0), (self.gx, h, self.gz))]
+        return blocks
+
+
+class BenchmarkZConnGeometry(LBGeometry3D):
+    def blocks(self, n=None):
+        d = self.gz / 2
+        blocks = [LBBlock3D((0, 0, 0), (self.gx, self.gy, d)),
+                LBBlock3D((0, 0, d), (self.gx, self.gy, d))]
+        return blocks
+
+def run_benchmark():
+    settings = {
+            'mode': 'benchmark',
+            'max_iters': 300,
+            'every': 100,
+            'blocks': 2,
+            'quiet': True,
+            'grid': 'D3Q19',
+            'gpus': [0,1],
+        }
+
+    fmt = ['%d', '%d', '%d', '%d', '%.6e', '%.6e', '%.6e', '%.6e']
+
+    def test_sizes(sizes, geo_cls):
+        for w, h, d in sizes:
+            print 'Testing {0} x {1} x {2}...'.format(w, h, d)
+            settings.update({'lat_nx': int(w), 'lat_ny': int(h), 'lat_nz': int(d)})
+            ctrl = LBSimulationController(LDCSim, geo_cls, settings)
+            timing_infos, blocks = ctrl.run()
+            summary.append(util.summarize(timing_infos, blocks))
+
+    summary = []
+    sizes = [(1276, 128, 124), (1148, 128, 138), (1020, 128, 155), (892, 128, 177),
+            (762, 256, 104), (636, 256, 124), (508, 256, 156), (380, 256, 207),
+            (252, 256, 314), (124, 512, 320)]
+    test_sizes(sizes, BenchmarkXConnGeometry)
+    np.savetxt('3d_2blocks_x.dat', summary, fmt)
+
+
+if __name__ == '__main__':
+    run_benchmark()
