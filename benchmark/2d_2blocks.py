34feaaa29e1fcc92a58265f6e26cf5bf41e9f672
Michał Januszewski <michalj@gmail.com>
2011-10-16 01:43:52 +0200

    Fix typos in the benchmark code.

--- benchmark/2d_2blocks.py
+++ benchmark/2d_2blocks.py
@@ -43,8 +43,7 @@ def run_benchmark(boundary_split=True, suffix=''):
         for w, h in sizes:
             print 'Testing {0} x {1}...'.format(w, h)
             settings.update({'lat_nx': int(w), 'lat_ny': int(h)})
-            ctrl = LBSimulationController(LDCSim, BenchmarkXConnGeometry,
-                    settings)
+            ctrl = LBSimulationController(LDCSim, geo_cls, settings)
             timing_infos, blocks = ctrl.run()
             summary.append(util.summarize(timing_infos, blocks))
             timings.append(timing_infos)
@@ -54,7 +53,7 @@ def run_benchmark(boundary_split=True, suffix=''):
     h = np.logspace(6, 15, 10, base=2).astype(np.uint32)
     w = mem_size / node_cost / h
     sizes = [(x, y) for x, y in zip(w, h)]
-    test_sizes(sizes, BenchmarkYConnGeometry)
+    test_sizes(sizes, BenchmarkXConnGeometry)
     np.savetxt('2d_2blocks_x{0}.dat'.format(suffix), summary, fmt)
     util.save_timing('2d_2blocks_x{0}.timing'.format(suffix), timings)
 
