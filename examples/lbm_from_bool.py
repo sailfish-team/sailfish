e1a178157968611cc08e1bb3a1104fe2e5a78327
Michał Januszewski <michalj@gmail.com>
2012-02-15 01:43:50 +0100

    Convert lbm_from_bool.

--- examples/lbm_from_bool.py
+++ examples/lbm_from_bool.py
@@ -1,70 +1,80 @@
 #!/usr/bin/python -u
-
-# Sample dummy simulation illustrating how to load geometry from a
-# boolean numpy array.
+"""Demonstrates how to load geometry from a boolean numpy array."""
 
 import sys
-import numpy
+import numpy as np
 
-from sailfish import geo, lb_single
+from sailfish.geo import LBGeometry3D
+from sailfish.geo_block import Subdomain3D
+from sailfish.controller import LBSimulationController
+from sailfish.lb_single import LBFluidSim, LBForcedSim
 
-import optparse
 
-class LBMGeo(geo.LBMGeo3D):
-    def define_nodes(self):
-        self.set_geo_from_bool_array(self.sim.geometry)
+class BoolSubdomain(Subdomain3D):
+    def initial_conditions(self, sim, hx, hy, hz):
+        sim.rho[:] = 1.0
 
-    def init_fields(self):
-        self.sim.rho[:] = 1.0
+    def boundary_conditions(self, hx, hy, hz):
+        if hasattr(self.config, 'wall_map'):
+            x0 = np.min(hx)
+            x1 = np.max(hx)
+            y0 = np.min(hy)
+            y1 = np.max(hy)
+            z0 = np.min(hz)
+            z1 = np.max(hz)
 
-class LSim(lb_single.FluidLBMSim):
-    filename = 'bool'
+            partial_wall_map = self.config.wall_map[z0:z1+1, y0:y1+1, x0:x1+1]
+            self.set_node(partial_wall_map, self.NODE_WALL)
 
-    def __init__(self, geo_class, args=sys.argv[1:], defaults=None):
-        opts = []
-        opts.append(optparse.make_option('--geo', dest='geo', type='string', help='file defining the geometry',
-                                         default=None))
 
-        lb_single.FluidLBMSim.__init__(self, geo_class, options=opts, args=args, defaults=defaults)
+class BoolSimulation(LBFluidSim, LBForcedSim):
+    subdomain = BoolSubdomain
 
-        if self.options.geo is not None:
-            geo = numpy.logical_not(numpy.load(self.options.geo))
-        else:
-            raise ValueError('Geometry file must be specified')
+    @classmethod
+    def add_options(cls, group, dim):
+        LBFluidSim.add_options(group, dim)
+        LBForcedSim.add_options(group, dim)
 
-        orig_shape = geo.shape
-        new_shape = list(orig_shape)
+        group.add_argument('--geometry', type=str,
+                help='file defining the geometry')
+
+    @classmethod
+    def modify_config(cls, config):
+        if not config.geometry:
+            return
+
+        wall_map = np.logical_not(np.load(config.geometry))
+        orig_shape = wall_map.shape
+        shape = list(wall_map.shape)
 
         # Add walls around the whole simulation domain.
-        new_shape[0] += 2
-        new_shape[1] += 2
-        new_shape[2] += 2
+        shape[0] += 2
+        shape[1] += 2
+        shape[2] += 2
 
         # Perform funny gymnastics to extend the array to the new shape.  numpy's
         # resize can only be used on the 1st axis if the position of the data in
         # the array is not to be changed.
-        geo = numpy.resize(geo, (new_shape[0], orig_shape[1], orig_shape[2]))
-        geo = numpy.rollaxis(geo, 1, 0)
-        geo = numpy.resize(geo, (new_shape[1], new_shape[0], orig_shape[2]))
-        geo = numpy.rollaxis(geo, 0, 2)
-        geo = numpy.rollaxis(geo, 2, 0)
-        geo = numpy.resize(geo, (new_shape[2], new_shape[0], new_shape[1]))
-        geo = numpy.rollaxis(geo, 0, 3)
+        wall_map = np.resize(wall_map, (shape[0], orig_shape[1], orig_shape[2]))
+        wall_map = np.rollaxis(wall_map, 1, 0)
+        wall_map = np.resize(wall_map, (shape[1], shape[0], orig_shape[2]))
+        wall_map = np.rollaxis(wall_map, 0, 2)
+        wall_map = np.rollaxis(wall_map, 2, 0)
+        wall_map = np.resize(wall_map, (shape[2], shape[0], shape[1]))
+        wall_map = np.rollaxis(wall_map, 0, 3)
 
-        geo[:,:,-2:] = True
-        geo[:,-2:,:] = True
-        geo[-2:,:,:] = True
+        wall_map[:,:,-2:] = True
+        wall_map[:,-2:,:] = True
+        wall_map[-2:,:,:] = True
 
         # Make sure the walls are _around_ the simulation domain.
-        geo = numpy.roll(geo, 1, 0)
-        geo = numpy.roll(geo, 1, 1)
-        geo = numpy.roll(geo, 1, 2)
+        wall_map = np.roll(wall_map, 1, 0)
+        wall_map = np.roll(wall_map, 1, 1)
+        wall_map = np.roll(wall_map, 1, 2)
 
-        self.options.lat_nx = geo.shape[2]
-        self.options.lat_ny = geo.shape[1]
-        self.options.lat_nz = geo.shape[0]
-        self.geometry = geo
+        config.lat_nz, config.lat_ny, config.lat_nx = wall_map.shape
+        config._wall_map = wall_map
+
 
 if __name__ == '__main__':
-    sim = LSim(LBMGeo)
-    sim.run()
+    LBSimulationController(BoolSimulation, LBGeometry3D).run()
