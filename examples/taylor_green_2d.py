4c8321c59cb053bb862850405a8436fe9c6d270e
Michał Januszewski <michalj@gmail.com>
2012-11-23 01:19:59 +0100

    Make the Tyalor-Green example more configurable.
    Also change the error output to be the relative error (in comparison to
    the analytical solution).

--- examples/taylor_green_2d.py
+++ examples/taylor_green_2d.py
@@ -2,7 +2,6 @@
 
 import math
 import numpy as np
-
 from sailfish.geo import EqualSubdomainsGeometry2D
 from sailfish.subdomain import Subdomain2D
 from sailfish.controller import LBSimulationController
@@ -10,14 +9,12 @@ from sailfish.lb_single import LBFluidSim
 
 
 class TaylorGreenSubdomain(Subdomain2D):
-    max_v = 0.05
-
     def boundary_conditions(self, hx, hy):
         pass
 
     def initial_conditions(self, sim, hx, hy):
         t = 0
-        Ma = self.max_v / math.sqrt(sim.grid.cssq)
+        Ma = self.config.max_v / math.sqrt(sim.grid.cssq)
         rho, vx, vy = self.solution(self.config, hx, hy, self.gx, self.gy, t, Ma)
         sim.rho[:] = rho
         sim.vx[:] = vx
@@ -28,12 +25,11 @@ class TaylorGreenSubdomain(Subdomain2D):
         """Returns the analytical solution of the 2D Taylor Green flow for time t."""
         kx = np.pi * 2.0 / (config.lambda_x * gx)
         ky = np.pi * 2.0 / (config.lambda_y * gy)
-
         ksq = np.square(kx) + np.square(ky)
         f = np.exp(-config.visc * ksq * t)
 
-        vx = -cls.max_v * ky / np.sqrt(ksq) * f * np.sin(ky * hy) * np.cos(kx * hx)
-        vy =  cls.max_v * kx / np.sqrt(ksq) * f * np.sin(kx * hx) * np.cos(ky * hy)
+        vx = -config.max_v * ky / np.sqrt(ksq) * f * np.sin(ky * hy) * np.cos(kx * hx)
+        vy =  config.max_v * kx / np.sqrt(ksq) * f * np.sin(kx * hx) * np.cos(ky * hy)
         rho = 1.0 - Ma**2 / (ksq * 2.0) * (
             ky**2 * np.cos(2 * kx * hx) +
             kx**2 * np.cos(2 * ky * hy))
@@ -51,33 +47,41 @@ class TaylorGreenSim(LBFluidSim):
             'lat_nx': 256,
             'lat_ny': 256,
             'visc': 0.001
-            })
+        })
 
     @classmethod
     def add_options(cls, group, dim):
         LBFluidSim.add_options(group, dim)
 
+        group.add_argument('--max_v', type=float, default=0.01)
         group.add_argument('--lambda_x', type=int, default=1)
         group.add_argument('--lambda_y', type=int, default=1)
+        group.add_argument('--err_every', type=int, default=100)
+
+    def __init__(self, config):
+        super(TaylorGreenSim, self).__init__(config)
 
     def after_step(self, runner):
-        every = 200
-
-        if self.iteration % (every - 1) == 0:
+        if self.iteration % self.config.err_every == self.config.err_every - 1:
             self.need_sync_flag = True
-        elif self.iteration % every == 0:
-            u = np.sqrt(runner._sim.vx**2 + runner._sim.vy**2)
+        elif self.iteration % self.config.err_every == 0:
+            vx = runner._sim.vx.astype(np.float64)
+            vy = runner._sim.vy.astype(np.float64)
+
             ny, nx = runner._sim.vx.shape
             hy, hx = np.mgrid[0:ny, 0:nx]
 
-            Ma = self.subdomain.max_v / math.sqrt(runner._sim.grid.cssq)
+            Ma = self.config.max_v / math.sqrt(runner._sim.grid.cssq)
             _, ref_ux, ref_uy = self.subdomain.solution(self.config, hx, hy,
                                                         nx, ny,
                                                         self.iteration, Ma)
-            ref_u = np.sqrt(ref_ux**2 + ref_uy**2)
-
-            du_norm = np.linalg.norm(u - ref_u) / u.size
-            print du_norm
+            top_v = np.max(np.sqrt(np.square(vx) + np.square(vy)))
+            top_ref_v = np.max(np.sqrt(np.square(ref_ux) + np.square(ref_uy)))
+            dx = ref_ux - vx
+            dy = ref_uy - vy
+            err = (np.linalg.norm(np.dstack([dx, dy])) /
+                   np.linalg.norm(np.dstack([ref_ux, ref_uy])))
+            print self.iteration, err, top_v, top_ref_v
 
 
 if __name__ == '__main__':
