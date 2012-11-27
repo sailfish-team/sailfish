a0f644c352d7b2d49d5a480f2bcb2a89c4094819
Michał Januszewski <michalj@gmail.com>
2012-11-27 03:40:50 +0100

    Add slice navigation to the cutplane visualization module.

--- sailfish/vis_3dcutplane.py
+++ sailfish/vis_3dcutplane.py
@@ -5,6 +5,8 @@ __email__ = 'sailfish-cfd@googlegroups.com'
 __license__ = 'LGPL3'
 
 import numpy as np
+import pygame
+
 from sailfish import vis_2d
 
 class Fluid3DVisCutplane(vis_2d.Fluid2DVis):
@@ -12,6 +14,8 @@ class Fluid3DVisCutplane(vis_2d.Fluid2DVis):
     dims = [3]
 
     def __init__(self, config, blocks, quit_event, sim_quit_event, vis_config):
+        self._slice_position = [0, 0, 0]
+        self._slice_axis = 0
         super(Fluid3DVisCutplane, self).__init__(config, blocks,
                                                  quit_event, sim_quit_event,
                                                  vis_config)
@@ -24,7 +28,15 @@ class Fluid3DVisCutplane(vis_2d.Fluid2DVis):
 
     @property
     def size(self):
-        return self._blocks[self._vis_config.block].size[:-1]
+        size = list(self._blocks[self._vis_config.block].size)
+        del size[self._slice_axis]
+        return size
+
+    @property
+    def selector(self):
+        sel = [slice(None), slice(None), slice(None)]
+        sel[(2 - self._slice_axis)] = self._slice_position[self._slice_axis]
+        return sel
 
     def size3d(self):
         return list(reversed(self._blocks[self._vis_config.block].size))
@@ -34,19 +46,45 @@ class Fluid3DVisCutplane(vis_2d.Fluid2DVis):
         t = np.zeros(size, dtype=np.uint8)
         t.ravel()[:] = block.vis_geo_buffer[:]
         geo_map = np.zeros((height, width), dtype=np.uint8)
-        geo_map[:] = t[5,:,:]
+        geo_map[:] = t[self.selector]
         return geo_map
 
     def _get_field(self, width, height, block):
         # FIXME(michalj): This is horribly inefficient.  We should only recreate
         # the array if the data has been updated.
-
         size = self.size3d()
         t = np.zeros(size, dtype=np.float32)
         t.ravel()[:] = block.vis_buffer[:]
 
         tmp = np.zeros((height, width), dtype=np.float32)
-        tmp[:] = t[5,:,:]
+        tmp[:] = t[self.selector]
         return tmp
+
+    def _visualize(self):
+        ret = super(Fluid3DVisCutplane, self)._visualize()
+        axes = ['x', 'y', 'z']
+        ret.append('slice @ {0} = {1}'.format(axes[self._slice_axis],
+                                              self._slice_position[self._slice_axis]))
+        return ret
+
+    def _process_misc_event(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_x:
+                self._slice_axis = 0
+                self.resize()
+            elif event.key == pygame.K_y:
+                self._slice_axis = 1
+                self.resize()
+            elif event.key == pygame.K_z:
+                self._slice_axis = 2
+                self.resize()
+            # Move the slice along the selected axis.
+            elif event.key == pygame.K_QUOTE:
+                if (self._slice_position[self._slice_axis] <
+                    self.size3d()[(2 - self._slice_axis)] - 1):
+                    self._slice_position[self._slice_axis] += 1
+            elif event.key == pygame.K_SEMICOLON:
+                if self._slice_position[self._slice_axis] > 0:
+                    self._slice_position[self._slice_axis] -= 1
 
 engine=Fluid3DVisCutplane
