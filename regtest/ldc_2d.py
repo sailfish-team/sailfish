0f4b5290526b4dd204b337ad6b9a60b2b619d9ca
Michał Januszewski <michalj@gmail.com>
2013-02-09 15:37:12 +0100

    Clean up the LDC 2d regression test.

--- regtest/ldc_2d.py
+++ regtest/ldc_2d.py
@@ -9,7 +9,7 @@ grid size: 601x601."""
 import numpy as np
 import matplotlib
 
-matplotlib.use('cairo')
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import os
 import shutil
@@ -25,33 +25,8 @@ from utils.merge_subdomains import merge_subdomains
 
 tmpdir = tempfile.mkdtemp()
 
-BLOCKS = 1
-LAT_NX = 512
-LAT_NY = 512
-output = ''
-RE = 1000
-MAX_ITERS = 500000
-
 max_iters = [500000, 1000000, 2000000, 3000000, 4000000, 4500000, 4500000, 5500000, 5500000, 6000000]
 reynolds = [1000, 2500, 5000, 7500, 10000, 12500, 15000, 17500, 20000, 21000]
-name = 'ldc2d'
-
-
-class TestLDCGeometry(LBGeometry2D):
-
-    def subdomains(self, n=None):
-        blocks = []
-        bps = self.config.blocks
-        yq = self.gy / bps
-        yd = self.gy % bps
-
-        for k in range(0, bps):
-            ysize = yq
-            if k == bps - 1:
-                ysize += yd
-            blocks.append(SubdomainSpec2D((0, k * yq),
-                                          (self.gx, ysize)))
-        return blocks
 
 
 class TestLDCSim(LDCSim):
@@ -61,32 +36,25 @@ class TestLDCSim(LDCSim):
         LDCBlock.max_v = 0.05
         LDCSim.update_defaults(defaults)
         defaults.update({
-            'max_iters': MAX_ITERS,
-            'lat_nx': LAT_NX,
-            'lat_ny': LAT_NY,
+            'quiet': True,
+            'lat_nx': 512,
+            'lat_ny': 512,
             'output': os.path.join(tmpdir, 'result')})
 
     @classmethod
     def modify_config(cls, config):
-        print config.re
         config.visc = (config.lat_nx - 2) * LDCBlock.max_v / config.re
         config.every = config.max_iters
-
-        # Protection in the event of max_iters changes from the command line.
-        global MAX_ITERS, BLOCKS
-        MAX_ITERS = config.max_iters
-        BLOCKS = config.blocks
-
 
     @classmethod
     def add_options(cls, group, dim):
         LDCSim.add_options(group, dim)
-        group.add_argument('--re', dest="re", help='Reynolds number', type=int, default=RE)
+        group.add_argument('--re', help='Reynolds number', type=int, default=0)
 
 
-def save_output(basepath):
+def save_output(basepath, max_iters):
     merged = merge_subdomains(os.path.join(tmpdir, 'result'),
-            io.filename_iter_digits(MAX_ITERS), MAX_ITERS, save=False)
+            io.filename_iter_digits(max_iters), max_iters, save=False)
 
     rho = merged['rho']
     lat_ny, lat_nx = rho.shape
@@ -107,30 +75,29 @@ def save_output(basepath):
     np.savetxt(os.path.join(basepath, 'vy.dat'), res_vy)
 
 
-def run_test(name, i):
-    global RE
-    RE = reynolds[i]
-    global MAX_ITERS
-    MAX_ITERS = max_iters[i]
-    basepath = os.path.join('results', name, 're%s' % RE)
+def run_test(name, re, max_iters, i):
+    print 'Testing Re = %s' % re
+
+    basepath = os.path.join('results', name, 're%s' % re)
     if not os.path.exists(basepath):
         os.makedirs(basepath)
 
-    ctrl = LBSimulationController(TestLDCSim, TestLDCGeometry)
-    ctrl.run()
+    ctrl = LBSimulationController(TestLDCSim,
+                                  default_config={'re': re, 'max_iters': max_iters})
+    ctrl.run(ignore_cmdline=True)
     horiz = np.loadtxt('ldc_golden/vx2d', skiprows=4)
     vert = np.loadtxt('ldc_golden/vy2d', skiprows=4)
 
-    plt.plot(horiz[:, 0] * 2 - 1, horiz[:, i+1], label='Paper')
-    plt.plot(vert[:, i+1], 2 * (vert[:, 0] - 0.5), label='Paper')
-    save_output(basepath)
+    plt.plot(horiz[:, 0] * 2 - 1, horiz[:, i], '.', label='Paper')
+    plt.plot(vert[:, i], 2 * (vert[:, 0] - 0.5), '.', label='Paper')
+    save_output(basepath, max_iters)
     plt.legend(loc='lower right')
     plt.gca().yaxis.grid(True)
     plt.gca().xaxis.grid(True)
     plt.gca().xaxis.grid(True, which='minor')
     plt.gca().yaxis.grid(True, which='minor')
 
-    plt.title('Lid Driven Cavity, Re = %s' % RE)
+    plt.title('2D Lid Driven Cavity, Re = %s' % re)
     print os.path.join(basepath, 'results.pdf')
     plt.savefig(os.path.join(basepath, 'results.pdf'), format='pdf')
 
@@ -139,6 +106,7 @@ def run_test(name, i):
     plt.show()
 
 
-for i in range(10):
-    run_test(name, i)
+for i, (re, max_it) in enumerate(zip(reynolds, max_iters)):
+    run_test('ldc2d', re, max_it, i+1)
+
 shutil.rmtree(tmpdir)
