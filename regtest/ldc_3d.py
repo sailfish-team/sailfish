8586e846c5eec79a574cf124e9e50492dee2e6a3
Michał Januszewski <michalj@gmail.com>
2013-02-10 23:43:42 +0100

    Minor cleanup in the ldc_3d testcase.

--- regtest/ldc_3d.py
+++ regtest/ldc_3d.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import numpy as np
 import matplotlib
 
-matplotlib.use('cairo')
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import os
 import shutil
@@ -12,38 +12,11 @@ import tempfile
 from examples.ldc_3d import LDCBlock, LDCSim
 from sailfish import io
 from sailfish.controller import LBSimulationController
-from sailfish.geo import LBGeometry3D
-from sailfish.subdomain import SubdomainSpec3D
 
 from utils.merge_subdomains import merge_subdomains
 
 tmpdir = tempfile.mkdtemp()
 MAX_ITERS = 100000
-LAT_NX = 256
-LAT_NY = 256
-LAT_NZ = 256
-BLOCKS = 1
-output = ''
-
-name = 'ldc3d'
-
-
-class TestLDCGeometry(LBGeometry3D):
-
-    def subdomains(self, n=None):
-        blocks = []
-        bps = self.config.blocks
-        zq = self.gz / bps
-        zd = self.gz % bps
-
-        for k in range(0, bps):
-            zsize = zq
-            if k == bps - 1:
-                zsize += zd
-            blocks.append(SubdomainSpec3D((0, 0, k * zq),
-                                          (self.gx, self.gy, zsize)))
-        return blocks
-
 
 class TestLDCSim(LDCSim):
 
@@ -53,9 +26,9 @@ class TestLDCSim(LDCSim):
         LDCSim.update_defaults(defaults)
         defaults.update({
             'max_iters': MAX_ITERS,
-            'lat_nx': LAT_NX,
-            'lat_ny': LAT_NY,
-            'lat_nz': LAT_NZ,
+            'lat_nx': 256,
+            'lat_ny': 256,
+            'lat_nz': 256,
             'output': os.path.join(tmpdir, 'result')})
 
     @classmethod
@@ -63,20 +36,15 @@ class TestLDCSim(LDCSim):
         config.visc = (config.lat_nx-2) * LDCBlock.max_v / config.re
         config.every = config.max_iters
 
-        # Protection in the event of max_iters changes from the command line.
-        global MAX_ITERS, BLOCKS
-        MAX_ITERS = config.max_iters
-        BLOCKS = config.blocks
-
     @classmethod
     def add_options(cls, group, dim):
         LDCSim.add_options(group, dim)
         group.add_argument('--re', dest="re", help = 'Reynolds number', type=int, default=400)
 
 
-def save_output(basepath):
+def save_output(basepath, max_iters):
     merged = merge_subdomains(os.path.join(tmpdir, 'result'),
-                    io.filename_iter_digits(MAX_ITERS), MAX_ITERS, save=False)
+                    io.filename_iter_digits(max_iters, max_iters, save=False)
 
     rho = merged['rho']
     lat_nz, lat_ny, lat_nx = rho.shape
@@ -104,14 +72,14 @@ def run_test(name):
     if not os.path.exists(basepath):
         os.makedirs(basepath)
 
-    ctrl = LBSimulationController(TestLDCSim, TestLDCGeometry)
+    ctrl = LBSimulationController(TestLDCSim)
     ctrl.run()
     horiz = np.loadtxt('ldc_golden/re400_horiz', skiprows=1)
     vert = np.loadtxt('ldc_golden/re400_vert', skiprows=1)
 
     plt.plot(2 * (horiz[:,0] - 0.5), -2 * (horiz[:,1] - 0.5), label='Sheu, Tsai paper')
     plt.plot(2 * (vert[:,0] - 0.5), -2 * (vert[:,1] - 0.5), label='Sheu, Tsai paper')
-    save_output(basepath)
+    save_output(basepath, MAX_ITERS)
     plt.legend(loc='lower right')
     plt.gca().yaxis.grid(True)
     plt.gca().xaxis.grid(True)
@@ -127,5 +95,4 @@ def run_test(name):
     plt.show()
     shutil.rmtree(tmpdir)
 
-
-run_test(name)
+run_test('ldc3d')
