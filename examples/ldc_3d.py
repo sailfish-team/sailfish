8603a51bcc40d0d93c4f183da05687854cf7b28d
Michał Januszewski <michalj@gmail.com>
2013-07-31 21:54:37 +0200

    Simplify the 3D lid-driven cavity example.

--- examples/ldc_3d.py
+++ examples/ldc_3d.py
@@ -1,47 +1,11 @@
 #!/usr/bin/env python
+"""3D lid-driven cavity."""
 
 import numpy as np
-from sailfish.geo import LBGeometry3D
-from sailfish.subdomain import SubdomainSpec3D, Subdomain3D
+from sailfish.subdomain import Subdomain3D
 from sailfish.node_type import NTFullBBWall, NTRegularizedVelocity
 from sailfish.controller import LBSimulationController
 from sailfish.lb_single import LBFluidSim
-
-
-class LDCGeometry(LBGeometry3D):
-    def subdomains(self, n=None):
-        subdomains = []
-        bps = int(self.config.ldc_subdomains**(1.0/3))
-
-        if bps**3 != self.config.ldc_subdomains:
-            print ('Only configurations with '
-                    'a third power of an integer number of subdomains are '
-                    'supported.  Falling back to {0} x {0} subdomains.'.
-                    format(bps))
-
-        xq = self.gx / bps
-        xd = self.gx % bps
-        yq = self.gy / bps
-        yd = self.gy % bps
-        zq = self.gz / bps
-        zd = self.gz % bps
-
-        for i in range(0, bps):
-            xsize = xq
-            if i == bps - 1:
-                xsize += xd
-            for j in range(0, bps):
-                ysize = yq
-                if j == bps - 1:
-                    ysize += yd
-                for k in range(0, bps):
-                    zsize = zq
-                    if k == bps - 1:
-                        zsize += zd
-                    subdomains.append(SubdomainSpec3D((i * xq, j * yq, k * zq),
-                                (xsize, ysize, zsize)))
-        return subdomains
-
 
 class LDCBlock(Subdomain3D):
     """3D Lid-driven geometry."""
@@ -74,13 +38,6 @@ class LDCSim(LBFluidSim):
             'lat_nz': 64,
             'grid': 'D3Q19'})
 
-    @classmethod
-    def add_options(cls, group, dim):
-        LBFluidSim.add_options(group, dim)
-
-        group.add_argument('--ldc_subdomains', type=int, default=1, help='number of subdomains to use')
-
-
 if __name__ == '__main__':
-    ctrl = LBSimulationController(LDCSim, LDCGeometry)
+    ctrl = LBSimulationController(LDCSim)
     ctrl.run()
