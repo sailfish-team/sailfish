b76d58c5470e6f54cbaacf17ce2fa973e38205b2
Michał Januszewski <michalj@gmail.com>
2013-09-22 20:08:39 +0200

    Add support for velocity scaling in utils/npz_to_vti.

--- utils/npz_to_vti.py
+++ utils/npz_to_vti.py
@@ -10,6 +10,7 @@ __author__ = 'Michal Januszewski'
 __email__ = 'sailfish-cfd@googlegroups.com'
 __license__ = 'LGPL3'
 
+import argparse
 import os
 import sys
 import numpy as np
@@ -17,8 +18,16 @@ import numpy as np
 os.environ['ETS_TOOLKIT'] = 'null'
 from tvtk.api import tvtk
 
-filename = sys.argv[1]
+parser = argparse.ArgumentParser()
+parser.add_argument('--dx', type=float, default=0.0)
+parser.add_argument('--dt', type=float, default=0.0)
+args, remaining = parser.parse_known_args()
+
+filename = remaining[0]
 src_data = np.load(filename)
+
+dx = 1.0 if args.dx == 0.0 else args.dx
+dt = 1.0 if args.dt == 0.0 else args.dt
 
 max_len = 0
 for field in src_data.files:
@@ -45,7 +54,7 @@ else:
     raise ValueError('Unexpected field shape length %d' % max_len)
 
 first = True
-idata = tvtk.ImageData(spacing=(1, 1, 1), origin=(0, 0, 0))
+idata = tvtk.ImageData(spacing=(dx, dx, dx), origin=(0, 0, 0))
 # Only process scalar fields.
 for field in src_data.files:
     if len(src_data[field].shape) == max_len:
@@ -67,12 +76,13 @@ for field in src_data.files:
     f = src_data[field]
 
     if dim == 3:
-        tmp = idata.point_data.add_array(np.c_[f[0].flatten(),
-                                         f[1].flatten(), f[2].flatten()])
+        tmp = idata.point_data.add_array(np.c_[f[0].flatten() * dx/dt,
+                                               f[1].flatten() * dx/dt,
+                                               f[2].flatten() * dx/dt])
     else:
-        tmp = idata.point_data.add_array(np.c_[f[0].flatten(),
-                                         f[1].flatten(),
-                                         np.zeros_like(f[0].flatten())])
+        tmp = idata.point_data.add_array(np.c_[f[0].flatten() * dx/dt,
+                                               f[1].flatten() * dx/dt,
+                                               np.zeros_like(f[0].flatten())])
     idata.point_data.get_array(tmp).name = field
 
 if dim == 3:
