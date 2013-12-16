34d9e4c612cdf749b4dcff540a1bd3f4be671b59
Michał Januszewski <michalj@gmail.com>
2013-12-16 03:17:28 +0100

    Update the duct_flow test case with code implementing the analytical solution.

--- examples/duct_flow.py
+++ examples/duct_flow.py
@@ -3,24 +3,15 @@
 """
 Velocity-driven flow through a duct of rectangular cross-section.
 
-Geometry of the flow is the same as:
-
-S. S. Chikatamarla1, S. Ansumali and I. V. Karlin
-Grad’s approximation for missing data in lattice Boltzmann simulations
-Europhys. Lett., 74 (2), pp. 215–221 (2006)
-
-and the simulation output can be used for the inflow profile of a
-backward-facing step simulation.
-
-TODO: Compare the results with the analytical solution for rectangular
-duct flow from F. M. White, "Viscous Fluid Flow", 2nd, p. 120, Eq. 3.48:
+This case has an analytical solution:
+(from F. M. White, "Viscous Fluid Flow", 2nd, p. 120, Eq. 3.48):
 
     -a <= y <= a
     -b <= z <= b
 
-    u(y, z) = \frac{16 a^2}{\mu \pi^3} \left(- \frac{dp}{dx} \right) \sum_{i =
-        1,3,5,...}^{\infty} (-1)^{(i-2)/2} \left(1 - \frac{\cosh(i \pi z / 2a)}{\cosh({i
-        \pi b / 2a)}} \right) \frac{\cos(i \pi y/2a)}{i^3}
+    u(y, z) = \\frac{16 a^2}{\mu \pi^3} \left(- \\frac{dp}{dx} \\right) \sum_{i =
+        1,3,5,...}^{\infty} (-1)^{(i-1)/2} \left(1 - \\frac{\cosh(i \pi z / 2a)}{\cosh({i
+        \pi b / 2a)}} \\right) \\frac{\cos(i \pi y/2a)}{i^3}
 """
 
 import numpy as np
@@ -28,43 +19,103 @@ import numpy as np
 from sailfish.geo import EqualSubdomainsGeometry3D
 from sailfish.subdomain import Subdomain3D
 from sailfish.controller import LBSimulationController
+from sailfish.lb_base import LBForcedSim
 from sailfish.lb_single import LBFluidSim
-from sailfish.node_type import NTFullBBWall, NTEquilibriumVelocity, NTGradFreeflow
+from sailfish.node_type import NTFullBBWall, NTHalfBBWall
 
 class DuctSubdomain(Subdomain3D):
     max_v = 0.02
+    wall_bc = NTHalfBBWall
 
     def boundary_conditions(self, hx, hy, hz):
-        wall_map = (hz == 0) | (hz == self.gz - 1) | (hx == 0) | (hx == self.gx - 1)
-        inflow_map = np.logical_not(wall_map) & (hy == 0)
-        outflow_map = np.logical_not(wall_map) & (hy == self.gy - 1)
-
-        self.set_node(wall_map, NTFullBBWall)
-        self.set_node(inflow_map, NTEquilibriumVelocity((0.0, self.max_v, 0.0)))
-        self.set_node(outflow_map, NTGradFreeflow)
+        wall_map = (hx == 0) | (hx == self.gx - 1) | (hy == 0) | (hy == self.gy - 1)
+        self.set_node(wall_map, self.wall_bc)
 
     def initial_conditions(self, sim, hx, hy, hz):
         sim.rho[:] = 1.0
-        sim.vy[:] = self.max_v
+        sim.vz[:] = self.analytical(hx, hy)
 
-class DuctSim(LBFluidSim):
+    @classmethod
+    def width(cls, config):
+        return config.lat_ny - 1 - 2 * cls.wall_bc.location
+
+    @classmethod
+    def accel(cls, config):
+        # The maximum velocity is attained at the center of the channel,
+        # i.e. x = y = 0.
+        ii = np.arange(1, 100, 2)
+        ssum = np.sum((-1)**((ii - 1)/2.0) * (1 - np.cosh(0) / np.cosh(ii * np.pi / 2)) *
+                      np.cos(0) / ii**3)
+        a = cls.width(config) / 2.0
+        prefactor = 16 * a**2 / (config.visc * np.pi**3)
+        return cls.max_v / (prefactor * ssum)
+
+    def analytical(self, hx, hy):
+        a = self.width(self.config) / 2.0
+        hy = hy - self.wall_bc.location
+        hx = hx - self.wall_bc.location
+        ry = np.abs(a - hy)
+        rx = np.abs(a - hx)
+
+        prefactor = 16 * a**2 / (self.config.visc * np.pi**3)
+        ii = np.arange(1, 100, 2)
+        ret = np.zeros_like(hy)
+        for i in ii:
+            ret += ((-1)**((i - 1)/2.0) *
+                (1 - np.cosh(i * np.pi * rx / (2.0 * a)) /
+                     np.cosh(i * np.pi / 2)) *
+                np.cos(i * np.pi * ry / (2.0 * a)) / i**3)
+
+        return self.accel(self.config) * prefactor * ret
+
+
+class DuctSim(LBFluidSim, LBForcedSim):
     subdomain = DuctSubdomain
 
     @classmethod
     def update_defaults(cls, defaults):
-        S = 10
         defaults.update({
-            'lat_nx': 36 * S + 2,
-            'lat_ny': 15 * S,
-            'lat_nz': 1 * S + 2,
-            'grid': 'D3Q15',
-            'visc': 0.01,
-            'max_iters': 100000,
-            'every': 10000,
-            'output': 'duct',
-            'checkpoint_file': 'duct.cpoint',
-            'final_checkpoint': True,
-            'checkpoint_every': 1000000000})
+            'lat_nx': 64,
+            'lat_ny': 64,
+            'lat_nz': 32,
+            'grid': 'D3Q19',
+            'visc': 0.0064,
+            'max_iters': 500000,
+            'every': 50000,
+            'output': 'square_duct',
+            'periodic_z': True,
+            'block_size': 128,
+            # Double precision is required for convergence studies.
+            'precision': 'double'
+        })
+
+    def __init__(self, config):
+        super(DuctSim, self).__init__(config)
+        # The flow is driven by a body force.
+        self.add_body_force((0.0, 0.0, DuctSubdomain.accel(config)))
+
+        a = DuctSubdomain.width(config) / 2.0
+        self.config.logger.info('Re = %.2f' % (a * DuctSubdomain.max_v / config.visc))
+
+    _ref = None
+    def after_step(self, runner):
+        every = 1000
+        mod = self.iteration % every
+
+        if mod == every - 1:
+            self.need_sync_flag = True
+        elif mod == 0:
+            # Cache the reference solution.
+            if self._ref is None:
+                hx, hy, hz = runner._subdomain._get_mgrid()
+                self._ref = runner._subdomain.analytical(hx, hy)
+
+            # Output data useful for monitoring the state of the simulation.
+            m = runner._output._fluid_map
+            l2 = (np.linalg.norm(self._ref[m] - runner._sim.vz[m]) /
+                  np.linalg.norm(self._ref[m]))
+            print self.iteration, l2, np.nanmax(runner._sim.vz)
+
 
 if __name__ == '__main__':
     ctrl = LBSimulationController(DuctSim, EqualSubdomainsGeometry3D)
