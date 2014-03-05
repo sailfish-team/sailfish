382f8838fdf46e6f332bc64812d5347ea11a4bf9
nlooije <nlooije@gmail.com>
2014-03-05 01:23:04 +0100

    added example case ternary fluid

--- examples/ternary_fluid/sc_drop_2d.py
+++ examples/ternary_fluid/sc_drop_2d.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 from sailfish.geo import LBGeometry2D
 from sailfish.subdomain import Subdomain2D
 from sailfish.controller import LBSimulationController
@@ -10,14 +13,25 @@ class DropSubdomain(Subdomain2D):
         pass
 
     def initial_conditions(self, sim, hx, hy):
-        drop_map1 = (self.gx/4-hx)**2 + (self.gy/4-hy)**2 <= 32**2
-        drop_map2 = (3*self.gx/4-hx)**2 + (3*self.gy/4-hy)**2 <= 32**2
-        sim.rho[:] = 2.15
+
+        radius = 32
+        drop_map1 = (hx-self.gx/4)**2 + (hy-self.gy/4)**2 <= radius**2
+        drop_map2 = (hx-3*self.gx/4)**2 + (hy-3*self.gy/4)**2 <= radius**2
+
+        sim.rho[:] = 2.0
         sim.phi[:] = 0.02
         sim.theta[:] = 0.02
-        sim.rho[drop_map1 & drop_map2] = 0.02
-        sim.phi[drop_map1] = 0.15
-        sim.theta[drop_map2] = 0.15
+
+        sim.rho[drop_map1] = 0.02
+        sim.phi[drop_map1] = 0.5
+        sim.theta[drop_map1] = 0.02
+
+        sim.rho[drop_map2] = 0.02
+        sim.phi[drop_map2] = 0.02
+        sim.theta[drop_map2] = 2.0
+
+        ny, nx = sim.rho.shape
+        hy, hx = np.mgrid[0:ny, 0:nx]
 
 class SCSim(LBTernaryFluidShanChen):
     subdomain = DropSubdomain
@@ -27,16 +41,35 @@ class SCSim(LBTernaryFluidShanChen):
         defaults.update({
             'lat_nx': 256,
             'lat_ny': 256,
-            'G12': -4.8,
-            'G13': -4.8,
-            'G23': 1.6,
+            'G11': -4.8,
+            'G33': -4.8,
             'visc': 1.0 / 6.0,
             'periodic_x': True,
             'periodic_y': True,
-            'sc_potential': 'classic',
-            'every': 20,
+            'sc_potential': 'classic'
             })
 
+    def after_step(self, runner):
+
+        every_n = 10000
+
+        if self.iteration % every_n == every_n - 1:
+            self.need_sync_flag = True
+
+        elif self.iteration % every_n == 0:
+            rho = runner._sim.rho.astype(np.float64)
+            phi = runner._sim.phi.astype(np.float64)
+            theta = runner._sim.theta.astype(np.float64)
+
+            ny, nx = runner._sim.rho.shape
+            hy, hx = np.mgrid[0:ny, 0:nx]
+
+            fig1 = plt.figure(1); fig1.clf();
+            plt.plot( hx[hx==hy], rho[hx==hy], 'r',
+                      hx[hx==hy], phi[hx==hy], 'g',
+                      hx[hx==hy], theta[hx==hy], 'b', )
+            plt.legend(['rho','phi','theta'])
+            fig1.savefig('profiles.png')
 
 if __name__ == '__main__':
     LBSimulationController(SCSim, LBGeometry2D).run()
