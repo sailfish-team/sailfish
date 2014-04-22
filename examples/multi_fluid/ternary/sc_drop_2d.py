9e2d322724ce33158e135a4076ccc56ffcbaa6d8
Niels Looije <nlooije@gmail.com>
2014-04-22 14:35:41 +0200

    fixed nits

--- examples/multi_fluid/ternary/sc_drop_2d.py
+++ examples/multi_fluid/ternary/sc_drop_2d.py
@@ -17,8 +17,8 @@ class DropSubdomain(Subdomain2D):
     def initial_conditions(self, sim, hx, hy):
         radius = 32
 
-        drop_map1 = (hx-self.gx/4)**2 + (hy-self.gy/4)**2 <= radius**2
-        drop_map2 = (hx-3*self.gx/4)**2 + (hy-3*self.gy/4)**2 <= radius**2
+        drop_map1 = (hx - self.gx / 4)**2 + (hy - self.gy / 4)**2 <= radius**2
+        drop_map2 = (hx - 3 * self.gx / 4)**2 + (hy - 3 * self.gy / 4)**2 <= radius**2
 
         sim.g0m0[:] = 2.0
         sim.g1m0[:] = 0.02
@@ -62,9 +62,9 @@ class SCSim(LBMultiFluidShanChen):
             hy, hx = np.mgrid[0:ny, 0:nx]
 
             fig1 = plt.figure(1); fig1.clf();
-            plt.plot( hx[hx==hy], rho0[hx==hy], 'r',
-                      hx[hx==hy], rho1[hx==hy], 'g',
-                      hx[hx==hy], rho2[hx==hy], 'b', )
+            plt.plot( hx[hx == hy], rho0[hx == hy], 'r',
+                      hx[hx == hy], rho1[hx == hy], 'g',
+                      hx[hx == hy], rho2[hx == hy], 'b', )
             plt.legend(['rho0','rho1','rho2'])
             fig1.savefig('profiles.png')
 
