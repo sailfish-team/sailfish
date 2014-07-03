77755371e622d62a01ba2c1f545ed29c20c9482c
Michał Januszewski <michalj@gmail.com>
2014-07-03 22:22:13 +0900

    Create a base class for ushape simulations.

---
+++ examples/showcases/hemodynamics/ushape_stationary.py
@@ -0,0 +1,19 @@
+from sailfish.controller import LBSimulationController
+from sailfish.geo import EqualSubdomainsGeometry3D
+
+import ushape_base
+
+class UshapeSim(ushape_base.UshapeBaseSim):
+    lb_v = 0.05
+
+    @classmethod
+    def update_defaults(cls, defaults):
+        super(UshapeSim, cls).update_defaults(defaults)
+        defaults.update({
+            'velocity': 'constant',
+
+            # For benchmarking, disable error checking.
+        })
+
+if __name__ == '__main__':
+    LBSimulationController(UshapeSim, EqualSubdomainsGeometry3D).run()
