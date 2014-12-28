36b3b03b8364dac3ddd57d74e520ae2617675457
Michał Januszewski <michalj@gmail.com>
2014-12-29 00:32:30 +0100

    Add a script for oscillatory ushape simulations.

---
+++ examples/showcases/hemodynamics/ushape_oscillatory.py
@@ -0,0 +1,21 @@
+from sailfish.controller import LBSimulationController
+from sailfish.geo import EqualSubdomainsGeometry3D
+
+import ushape_base
+
+class UshapeSim(ushape_base.UshapeBaseSim):
+    lb_v = 0.001
+
+    @classmethod
+    def update_defaults(cls, defaults):
+        super(UshapeSim, cls).update_defaults(defaults)
+        defaults.update({
+            'velocity': 'oscillatory',
+            'model': 'mrt',
+            'reynolds': 100,
+
+            # For benchmarking, disable error checking.
+        })
+
+if __name__ == '__main__':
+    LBSimulationController(UshapeSim, EqualSubdomainsGeometry3D).run()
