2c4b22f7bf144cd79abd7f7c57ce0a2b6eef7ae0
Michał Januszewski <michalj@gmail.com>
2015-05-20 01:50:05 +0200

    Enable round off minimization by default in oscillatory ushape. Change the location of the init data, clean up rescaling code, and add a comment explaining why this is better than oscillatory delay.

--- examples/showcases/hemodynamics/ushape_oscillatory.py
+++ examples/showcases/hemodynamics/ushape_oscillatory.py
@@ -9,17 +9,23 @@ class OscillatoryUshapeSubdomain(ushape_base.UshapeSubdomain):
     oscillatory_delay = 0 #100000
 
     def initial_conditions(self, sim, hx, hy, hz):
-        a = np.load('results/ushape/old/ushape_re100_inc_500.0.150000.npz')
+        # Oscillatory delay also works, but convergence is slower. At 1M
+        # iterations, distortions from the original pressure wave are still
+        # visible.
+        a = np.load('results/ushape/ushape_re100_inc_500_mv0.17/data.0.75000.npz')
         v = a['v']
         v[np.isnan(v)] = 0.0
         rho = a['rho']
         rho[np.isnan(rho)] = 1.0
 
-        sim.vx[:] = v[0,:,:,:] / 0.05 * UshapeSim.lb_v
-        sim.vy[:] = v[1,:,:,:] / 0.05 * UshapeSim.lb_v
-        sim.vz[:] = v[2,:,:,:] / 0.05 * UshapeSim.lb_v
-        sim.rho[:] = (rho - 1.0) * (UshapeSim.lb_v / 0.05)**2  + 1.0
+        old_v_max = 0.17
 
+        sim.vx[:] = v[0,:,:,:] / old_v_max * UshapeSim.lb_v
+        sim.vy[:] = v[1,:,:,:] / old_v_max * UshapeSim.lb_v
+        sim.vz[:] = v[2,:,:,:] / old_v_max * UshapeSim.lb_v
+        sim.rho[:] = (rho - 1.0) * (UshapeSim.lb_v / old_v_max)**2  + 1.0
+
+        assert np.all(np.isfinite(sim.rho[:]))
 
 class UshapeSim(ushape_base.UshapeBaseSim):
     lb_v = 0.00025
@@ -33,6 +39,9 @@ class UshapeSim(ushape_base.UshapeBaseSim):
             'velocity': 'oscillatory',
             #'model': 'mrt',
             'reynolds': 100,
+            # Necessary since density variance in the simulation is of the order
+            # of machine epsilon in single precision.
+            'minimize_roundoff': True
 
             # For benchmarking, disable error checking.
         })
