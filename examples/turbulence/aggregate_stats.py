0fb9c76419bf2e1dcd01d94ae3ce5dd00846dc8d
Michał Januszewski <michalj@gmail.com>
2015-06-25 12:45:44 +0900

    Support weighted stats aggregation.

--- examples/turbulence/aggregate_stats.py
+++ examples/turbulence/aggregate_stats.py
@@ -5,25 +5,34 @@
 #
 # Usage:
 #  ./agregate_stats.py <output> <input ...>
+#
+# <input> can be filenames or filename,weight. The weights
+# are automatically normalized.
+
 
 from collections import defaultdict
 import numpy as np
 import sys
 
 data = {}
-cnt = 0
+weight_sum = 0.0
 
 for fname in sys.argv[2:]:
+    fname, _, weight = fname.partition(',')
+    if weight:
+        weight = float(weight)
+    else:
+        weight = 1.0
     a = np.load(fname)
     print "\r", fname, np.max(a['uz_m1']),
     for field in a.files:
         if field in data:
-            data[field] += a[field]
+            data[field] += weight * a[field]
         else:
-            data[field] = a[field]
-    cnt += 1
+            data[field] = weight * a[field]
+    weight_sum += 1
 
 for v in data.itervalues():
-    v /= cnt
+    v /= weight_sum
 
 np.savez(sys.argv[1], **data)
